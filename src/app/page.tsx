df52a82a470a2a0d333ca5e7c451cf2c2a0fb813
Onodwa2001 <onodwasiyotula7@gmail.com>
2024-05-05 00:11:29 +0200

    UI tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,60 @@
+"use client";
+import Navbar from "@/components/Navbar";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import Font Awesome styles
+import './globals.css';
+
+config.autoAddCss = false;
 
 export default function Home() {
+  const router = useRouter();
+  const words = [
+    {
+      text: "Find",
+    },
+    {
+      text: "nearby",
+    },
+    {
+      text: "tutors easily",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "Phoenix Tutorium.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+
   return (
     <div className="page">
 
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Homepage</a></li>
-              <li><a>Portfolio</a></li>
-              <li><a>About</a></li>
-            </ul>
+      <div className="">
+
+        <Navbar />
+
+        <div className="flex flex-col items-center justify-center h-[40rem]  ">
+          <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+            The road to knowledge starts here
+          </p>
+          <TypewriterEffectSmooth words={words} />
+          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+            <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm"
+              onClick={() => {
+                router.push('/search');
+              }}
+            >
+              Find Tutor
+            </button>
+            <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+              Signup
+            </button>
           </div>
-        </div>
-        <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
         </div>
       </div>
 
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,114 @@
+import Navbar from "@/components/Navbar";
+import { faCity, faFilter, faFunnelDollar, faHouse, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons/faCoffee";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function SearchPage() {
+
+    const card = {
+        padding: 20,
+        boxShadow: '0 8px 12px 0 rgba(0, 0, 0, 0.2)',
+        borderRadius: 8,
+        marginBottom: 20
+    }
+
+    const filter = {
+        marginRight: 20,
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: 'rgb(59, 130, 246)',
+        borderRadius: 100,
+        color: 'white',
+        cursor: 'pointer'
+    }
+
     return (
-        <>hello search</>
+
+        <div className="page" style={{
+            display: 'flex',
+        }}>
+            <div className="left sidebar" style={{
+                width: '15%',
+                height: '100vh',
+                backgroundColor: 'rgb(59, 130, 246)',
+                position: 'fixed',
+                padding: 20
+            }}>
+                <h1 style={{ fontWeight: 1000, color: 'white', fontSize: 18 }}>Phoenix Tutorium</h1>
+
+                <h2 style={{ color: 'white', fontWeight: 500, marginTop: 20 }}>Filters</h2>
+
+                <h3 style={{ color: 'white', fontWeight: 300, marginTop: 40, marginBottom: 5 }}>City</h3>
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Search City</option>
+                    <option value="1">Han Solo</option>
+                    <option value="1">Greedo</option>
+                </select>
+                <label htmlFor="city" style={{ color: 'white', marginRight: 10 }}>Add city filter</label>
+                <input type="checkbox" name="city" id="" />
+
+                <h3 style={{ color: 'white', fontWeight: 300, marginTop: 40, marginBottom: 5 }}>Suburb</h3>
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Search Suburb</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+                <label htmlFor="suburb" style={{ color: 'white', marginRight: 10 }}>Add suburb filter</label>
+                <input type="checkbox" name="suburb" id="" />
+
+                <h3 style={{ color: 'white', fontWeight: 300, marginTop: 40, marginBottom: 5 }}>Charging rate (hour)</h3>
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Choose range</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+                <label htmlFor="suburb" style={{ color: 'white', marginRight: 10 }}>Add suburb filter</label>
+                <input type="checkbox" name="suburb" id="" />
+                
+            </div>
+
+            <div className="right" style={{ 
+                width: '85%',
+                marginLeft: '15%'
+            }}>
+                <div className="navwrapper" style={{ position: "fixed", width: '85%' }}>
+                    <Navbar />
+                </div>
+
+                <div className="search-content" style={{
+                    padding: 80,
+                    paddingTop: 100
+                }}>
+                    <div className="filterOptions" style={{
+                        display: 'flex',
+                        marginRight: 10,
+                        marginBottom: 20
+                    }}>
+                        <div className="filter" style={filter}>Cape Town <span>x</span></div>
+                        <div className="filter" style={filter}>Khayelitsha <span>x</span></div>
+                        <div className="filter" style={filter}>200-300 <span>x</span></div>
+                    </div>
+
+                    <h1 style={{ fontSize: 22 }}>Results: 3</h1>
+                    <div className="search-results">
+                        <div className="tutor-card" style={card}>
+                            <h1>Mike James</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, enim recusandae. Placeat corrupti libero atque ullam ut, labore velit aliquam architecto consectetur veniam et ipsum deleniti tempora eaque nam? Ipsa.</p>
+                        </div>
+                        <div className="tutor-card" style={card}>
+                            <h1>Mike James</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, enim recusandae. Placeat corrupti libero atque ullam ut, labore velit aliquam architecto consectetur veniam et ipsum deleniti tempora eaque nam? Ipsa.</p>
+                        </div>
+                        <div className="tutor-card" style={card}>
+                            <h1>Mike James</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, enim recusandae. Placeat corrupti libero atque ullam ut, labore velit aliquam architecto consectetur veniam et ipsum deleniti tempora eaque nam? Ipsa.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
